dde8f4d5ed36486bfeb59c0d727ca61f201979cd
JordyCM24 <jordycalderon215@gmail.com>
2024-08-31 18:25:38 -0500

    localStorage persistente

--- src/app/countries/services/country.service.ts
+++ src/app/countries/services/country.service.ts
@@ -18,7 +18,19 @@ export class CountryService {
         byRegion: { region: '', countries: []},
     }
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { 
+        this.loadFromLocalStorage();
+    }
+
+    private saveToLocalStorage(){
+        localStorage.setItem('cacheStore', JSON.stringify(this.cacheStore));
+    }
+
+    private loadFromLocalStorage(){
+        if (!localStorage.getItem('cacheStore')) return;
+
+        this.cacheStore = JSON.parse(localStorage.getItem('cacheStore')!);
+    }
 
     // Voy a retornar un Observable de tipo Country[] y al get tambien le paso el tipo de dato que va a retornar
     private getCountriesRequest(url: string): Observable<Country []> {
@@ -44,7 +56,8 @@ export class CountryService {
         const url = `${this.apiUrl}/capital/${term}`;
         return this.getCountriesRequest(url)
         .pipe(
-            tap( countries => this.cacheStore.byCapital = {term, countries})
+            tap( countries => this.cacheStore.byCapital = {term, countries}),
+            tap(()=> this.saveToLocalStorage())
         );
     }
 
@@ -52,7 +65,8 @@ export class CountryService {
         const url = `${this.apiUrl}/name/${term}`;
         return this.getCountriesRequest(url)
         .pipe(
-            tap( countries => this.cacheStore.byCountries = {term, countries})
+            tap( countries => this.cacheStore.byCountries = {term, countries}),
+            tap(()=> this.saveToLocalStorage())
         );
     }
 
@@ -60,7 +74,8 @@ export class CountryService {
         const url = `${this.apiUrl}/region/${region}`;
         return this.getCountriesRequest(url)
         .pipe(
-            tap( countries => this.cacheStore.byRegion = {region, countries})
+            tap( countries => this.cacheStore.byRegion = {region, countries}),
+            tap(()=> this.saveToLocalStorage())
         );
     }
 
